0172ce14d23bc575f72955eea4b307505df79507
ChaosEngine <andrzej.pauli@gmail.com>
2017-05-03 21:23:43 +0200

    Fixed UPDATEs and UPSERT for mysql compatible sql. Added usings for mysql commands and readers.

--- src/Pomelo.Extensions.Caching.MySql/MySqlQueries.cs
+++ src/Pomelo.Extensions.Caching.MySql/MySqlQueries.cs
@@ -1,94 +1,89 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.Extensions.Internal;
-
 namespace Pomelo.Extensions.Caching.MySql
 {
-    internal class MySqlQueries
-    {
-        private const string TableInfoFormat =
-            "SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE " +
-            "FROM INFORMATION_SCHEMA.TABLES " +
-            "WHERE TABLE_SCHEMA = '{0}' " +
-            "AND TABLE_NAME = '{1}'";
+	internal class MySqlQueries
+	{
+		private const string TableInfoFormat =
+			"SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, TABLE_TYPE " +
+			"FROM INFORMATION_SCHEMA.TABLES " +
+			"WHERE TABLE_SCHEMA = '{0}' " +
+			"AND TABLE_NAME = '{1}'";
 
-        private const string UpdateCacheItemFormat =
-        "UPDATE {0} " +
-        "SET ExpiresAtTime = " +
-            "(CASE " +
-                "WHEN DATEDIFF(SECOND, @UtcNow) <= SlidingExpirationInSeconds " +
-                "THEN AbsoluteExpiration " +
-                "ELSE " +
-                "DATE_ADD(SECOND,INTERVAL @UtcNow SECONDS) " +
-            "END) " +
-        "WHERE Id = @Id " +
-        "AND @UtcNow <= ExpiresAtTime " +
-        "AND SlidingExpirationInSeconds IS NOT NULL " +
-        "AND (AbsoluteExpiration IS NULL OR AbsoluteExpiration <> ExpiresAtTime) ;";
+		private const string UpdateCacheItemFormat =
+		"UPDATE {0} " +
+		"SET ExpiresAtTime = " +
+			"(CASE " +
+				"WHEN TIME_TO_SEC(TIMEDIFF(@UtcNow, AbsoluteExpiration)) <= SlidingExpirationInSeconds " +
+					"THEN AbsoluteExpiration " +
+				"ELSE " +
+					"DATE_ADD(@UtcNow, INTERVAL SlidingExpirationInSeconds SECOND) " +
+			"END) " +
+		"WHERE Id = @Id " +
+		"AND @UtcNow <= ExpiresAtTime " +
+		"AND SlidingExpirationInSeconds IS NOT NULL " +
+		"AND (AbsoluteExpiration IS NULL OR AbsoluteExpiration <> ExpiresAtTime);";
 
-        private const string GetCacheItemFormat =
-            "SELECT Id, ExpiresAtTime, SlidingExpirationInSeconds, AbsoluteExpiration, Value " +
-            "FROM {0} WHERE Id = @Id AND @UtcNow <= ExpiresAtTime;";
+		private const string GetCacheItemFormat =
+			"SELECT Id, ExpiresAtTime, SlidingExpirationInSeconds, AbsoluteExpiration, Value " +
+			"FROM {0} WHERE Id = @Id AND @UtcNow <= ExpiresAtTime;";
 
-        private const string SetCacheItemFormat =
-            "DECLARE @ExpiresAtTime DATETIME; " +
-            "SET @ExpiresAtTime = " +
-            "(CASE " +
-                    "WHEN (@SlidingExpirationInSeconds IS NUll) " +
-                    "THEN @AbsoluteExpiration " +
-                    "ELSE " +
-                    "DATE_ADD(SECOND,INTERVAL @UtcNow SECONDS) " +
-            "END);" +
-            "UPDATE {0} SET Value = @Value, ExpiresAtTime = @ExpiresAtTime," +
-            "SlidingExpirationInSeconds = @SlidingExpirationInSeconds, AbsoluteExpiration = @AbsoluteExpiration " +
-            "WHERE Id = @Id " +
-            "IF (@@ROWCOUNT = 0) " +
-            "BEGIN " +
-                "INSERT INTO {0} " +
-                "(Id, Value, ExpiresAtTime, SlidingExpirationInSeconds, AbsoluteExpiration) " +
-                "VALUES (@Id, @Value, @ExpiresAtTime, @SlidingExpirationInSeconds, @AbsoluteExpiration); " +
-            "END ";
+		private const string SetCacheItemFormat =
+			"SET @ExpiresAtTime = " +
+			"(CASE " +
+					"WHEN (@SlidingExpirationInSeconds IS NUll) " +
+						"THEN @AbsoluteExpiration " +
+					"ELSE " +
+						"DATE_ADD(@UtcNow, INTERVAL @SlidingExpirationInSeconds SECOND) " +
+			"END);" +
+			 "INSERT INTO {0} " +
+			 "(Id, Value, ExpiresAtTime, SlidingExpirationInSeconds, AbsoluteExpiration) " +
+			 "VALUES (@Id, @Value, @ExpiresAtTime, @SlidingExpirationInSeconds, @AbsoluteExpiration)" +
+			" ON DUPLICATE KEY " +
+					  "UPDATE Value = @Value, ExpiresAtTime = @ExpiresAtTime," +
+					  "SlidingExpirationInSeconds = @SlidingExpirationInSeconds, AbsoluteExpiration = @AbsoluteExpiration " +
+					  ";";
 
-        private const string DeleteCacheItemFormat = "DELETE FROM {0} WHERE Id = @Id";
+		private const string DeleteCacheItemFormat = "DELETE FROM {0} WHERE Id = @Id";
 
-        public const string DeleteExpiredCacheItemsFormat = "DELETE FROM {0} WHERE @UtcNow > ExpiresAtTime";
+		public const string DeleteExpiredCacheItemsFormat = "DELETE FROM {0} WHERE @UtcNow > ExpiresAtTime";
 
-        public MySqlQueries(string schemaName, string tableName)
-        {
-            var tableNameWithSchema = string.Format(
-                "{0}.{1}", DelimitIdentifier(schemaName), DelimitIdentifier(tableName));
+		public MySqlQueries(string schemaName, string tableName)
+		{
+			var tableNameWithSchema = string.Format(
+				"{0}.{1}", DelimitIdentifier(schemaName), DelimitIdentifier(tableName));
 
-            // when retrieving an item, we do an UPDATE first and then a SELECT
-            GetCacheItem = string.Format(UpdateCacheItemFormat + GetCacheItemFormat, tableNameWithSchema);
-            GetCacheItemWithoutValue = string.Format(UpdateCacheItemFormat, tableNameWithSchema);
-            DeleteCacheItem = string.Format(DeleteCacheItemFormat, tableNameWithSchema);
-            DeleteExpiredCacheItems = string.Format(DeleteExpiredCacheItemsFormat, tableNameWithSchema);
-            SetCacheItem = string.Format(SetCacheItemFormat, tableNameWithSchema);
-            TableInfo = string.Format(TableInfoFormat, EscapeLiteral(schemaName), EscapeLiteral(tableName));
-        }
+			// when retrieving an item, we do an UPDATE first and then a SELECT
+			GetCacheItem = string.Format(UpdateCacheItemFormat + GetCacheItemFormat, tableNameWithSchema);
+			GetCacheItemWithoutValue = string.Format(UpdateCacheItemFormat, tableNameWithSchema);
+			DeleteCacheItem = string.Format(DeleteCacheItemFormat, tableNameWithSchema);
+			DeleteExpiredCacheItems = string.Format(DeleteExpiredCacheItemsFormat, tableNameWithSchema);
+			SetCacheItem = string.Format(SetCacheItemFormat, tableNameWithSchema);
+			TableInfo = string.Format(TableInfoFormat, EscapeLiteral(schemaName), EscapeLiteral(tableName));
+		}
 
-        public string TableInfo { get; }
+		public string TableInfo { get; }
 
-        public string GetCacheItem { get; }
+		public string GetCacheItem { get; }
 
-        public string GetCacheItemWithoutValue { get; }
+		public string GetCacheItemWithoutValue { get; }
 
-        public string SetCacheItem { get; }
+		public string SetCacheItem { get; }
 
-        public string DeleteCacheItem { get; }
+		public string DeleteCacheItem { get; }
 
-        public string DeleteExpiredCacheItems { get; }
+		public string DeleteExpiredCacheItems { get; }
 
-        // From EF's MySqlServerQuerySqlGenerator
-        private string DelimitIdentifier(string identifier)
-        {
-            return "[" + identifier.Replace("]", "]]") + "]";
-        }
+		// From EF's MySqlServerQuerySqlGenerator
+		private string DelimitIdentifier(string identifier)
+		{
+			return identifier;
+		}
 
-        private string EscapeLiteral(string literal)
-        {
-            return literal.Replace("'", "''");
-        }
-    }
+		private string EscapeLiteral(string literal)
+		{
+			return literal.Replace("'", "''");
+		}
+	}
 }
